1f3ed451dc82e07d3b52a97ac203eb15a24d501c
Ayaan Amjad <118003813+Ayaan-tech@users.noreply.github.com>
2024-07-30 20:24:45 +0530

    Create Day_11_06  Permutations_in_an_array.java
    Added the 6th Problem of Day_11 Set

---
+++ Day_11_06  Permutations_in_an_array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPossible(long a[], long b[], int n, long k) {
+      //Time Complexity : O(N*logN)
+      //Space Complexity :O(1)
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        for (int i=0;i<n;i++){
+            if(a[i] + b[n-1-i] < k){
+                return false;
+            }
+        }
+        return true;
+    }
+}
